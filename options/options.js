9ecf6f66753c573cc9981126fd404b72c4b83c85
Crowdbotics <engineering@crowdbotics.com>
2023-09-28 02:23:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bookfriend-dev-118813",
+  url: "https://bookfriend-dev-118813.botics.co",
+  api: "https://bookfriend-dev-118813.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
